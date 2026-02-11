275fc42c4d48a22f47f2d84e9cfecc199267db62
parveenstk <parveen.kumar@suretekinfosoft.com>
2026-02-11 17:19:05 +0530

    first name & last name maxlength updated V.1.2

--- app/pages/[lp].vue
+++ app/pages/[lp].vue
@@ -592,7 +592,7 @@ onMounted(async () => {
                                     <div class="mb-0 mt-4 grid grid-cols-1 md:grid-cols-2 gap-4">
                                         <!-- Billing - First Name -->
                                         <FormInput v-model="formFields.billingFirstName" name="billingFirstName"
-                                            :maxlength=16 placeholder="First Name" :error="errors.billingFirstName"
+                                            :maxlength=26 placeholder="First Name" :error="errors.billingFirstName"
                                             @input="validateField('billingFirstName', $event)" />
 
                                         <!-- Billing - Last Name -->
